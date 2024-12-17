40efe0b87a0808b0e0d0ea7a7afa219bb56f321b
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-12-17 20:51:52 +0800

    Update

---
+++ smallvec_cdf.py
@@ -0,0 +1,59 @@
+import json
+import numpy
+from matplotlib import pyplot as plt
+
+prefix = "/home/dtcxzyw/WorkSpace/Projects/compilers/llvm-project/"
+threshold = 10000
+with open('build/smallvec_dist.json', 'r') as f:
+    dist = json.load(f)
+
+advs = dict()
+for k, v in dist.items():
+    k = k.removeprefix(prefix)
+    arr = []
+    sum = 0
+    pairs = []
+    p = 0.95
+    cnt_sum = 0
+    for size, cnt in v.items():
+        pairs.append((int(size), cnt))
+        cnt_sum += cnt
+    if cnt_sum < threshold:
+        continue
+    tgt_sum = cnt_sum * p
+    pairs.sort(key=lambda x: x[0])
+    dest = None
+    for size, cnt in pairs:
+        while len(arr) <= size:
+            arr.append(sum)
+        sum += cnt
+        if dest is None and sum >= tgt_sum:
+            dest = size
+        arr[size] = sum
+    default = int(k.split(' ')[1])
+    if dest <= default:
+        continue
+    dest = int(2**numpy.ceil(numpy.log2(dest)))
+    p0 = arr[default]/cnt_sum*100
+    p1 = arr[min(dest, len(arr) - 1)]/cnt_sum*100
+    key = k[:k.find('.cpp')]
+    adv = (k, default, p0, dest, p1)
+    if key in advs:
+        advs[key].append(adv)
+    else:
+        advs[key] = [adv]
+    # suggest = f"{k}(p = {p0:.2f}) suggested size {dest}(p = {p1:.2f})"
+    # print(suggest)
+    # plt.title(suggest)
+    # y = numpy.array(arr)
+    # x = numpy.arange(len(y))
+    # plt.plot(x, y, label=k)
+    # plt.axvline(default, color='red')
+    # plt.axvline(dest, color='green')
+    # plt.waitforbuttonpress()
+    # plt.clf()
+
+for key, val in advs.items():
+    val.sort(key=lambda x: x[4]/max(0.001,x[2]))
+    for v in val:
+        print(f"{v[0]}(p = {v[2]:.2f}) suggested size {v[3]}(p = {v[4]:.2f})")
---
+++ smallvec_dist.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import subprocess
+import tqdm
+import json
+from multiprocessing import Pool
+
+llvm_bin = sys.argv[1]
+path = sys.argv[2]
+threads = int(sys.argv[3])
+
+work_list = []
+
+for r,ds,fs in os.walk(path):
+    if '/original' not in r:
+        continue
+    for f in fs:
+        if f.endswith('.ll'):
+            test_file = os.path.join(r,f)
+            work_list.append(test_file)
+work_list = work_list[:100]
+
+def extract_dist(err: str):
+    dist = dict()
+    for line in err.splitlines():
+        if line.startswith('SmallVec '):
+            line = line.removeprefix('SmallVec ')
+            pos = line.find(' ')
+            size = int(line[:pos])
+            key = line[pos+1:]
+            tmp = dist.get(key, dict())
+            tmp[size] = tmp.get(size, 0) + 1
+            dist[key] = tmp
+
+    return dist
+
+def verify(test_file):
+    try:
+        ret = subprocess.run([llvm_bin+"/opt", "-O3", "--disable-output", "--dump-small-vec-size", test_file], capture_output=True, timeout=30)
+        if ret.returncode == 0:
+            err = ret.stderr.decode('utf-8')
+            dist = extract_dist(err)
+            return (test_file, dist)
+        # print(ret.returncode)
+        return (test_file, None)
+    except subprocess.TimeoutExpired:
+        return (test_file, "timeout")
+    except Exception as e:
+        print(e)
+        pass
+
+    return (test_file, None)
+
+pool = Pool(processes=threads)
+
+progress = tqdm.tqdm(work_list)
+dist = dict()
+timeout_cnt = 0
+for test_file, res in pool.imap_unordered(verify, work_list):
+    progress.update()
+    if res is None:
+        progress.write(test_file + '\n')
+        exit(0)
+    if type(res) is str:
+        timeout_cnt += 1
+        progress.set_description(f"Timeout: {timeout_cnt}")
+        continue
+    for k, v in res.items():
+        tmp = dist.get(k, dict())
+        for size, cnt in v.items():
+            tmp[size] = tmp.get(size, 0) + cnt
+        dist[k] = tmp
+progress.close()
+
+with open('build/smallvec_dist.json', 'w') as f:
+    json.dump(dist, f)
+
+print("Timeout", timeout_cnt)
