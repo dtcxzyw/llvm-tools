8c0816b915417b9e033c08a80bf7d84d16485eb8
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-02-01 00:16:58 +0800

    Update

---
+++ rename.py
@@ -0,0 +1,40 @@
+import unidiff
+import os
+import sys
+
+file = sys.argv[1]
+diff = open(file).read()
+patch = unidiff.PatchSet(diff)
+
+
+def extract_name(line):
+    if line.startswith("%") or line.startswith("@") or line.startswith("!"):
+        pos = line.find(" =")
+        if pos != -1:
+            return (line[0], line[:pos].strip())
+        return None
+    pos = line.find(":")
+    if pos != -1:
+        return (":", line[:pos].strip())
+    return None
+
+
+for file in patch:
+    mapping = dict()
+    for hunk in file:
+        added = dict()
+        removed = dict()
+        for line in hunk:
+            if line.is_added:
+                added[line.target_line_no - hunk.target_start] = line.value
+            if line.is_removed:
+                removed[line.source_line_no - hunk.source_start] = line.value
+        for k, v in added.items():
+            if k in removed:
+                a = extract_name(removed[k].strip())
+                b = extract_name(v.strip())
+                if a is not None and b is not None and a[0] == b[0] and a[1] != b[1]:
+                    if b[1] not in mapping:
+                        mapping[b[1]] = {a[1]}
+                    else:
+                        mapping[b[1]].add(a[1])
